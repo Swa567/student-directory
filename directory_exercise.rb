0f8daa7aaccda82745a165d940151a1587c06869
Sarah <dousse.sarah@gmail.com>
2021-11-10 15:10:32 +0000

    exercice 6: centering output string

--- directory_exercise.rb
+++ directory_exercise.rb
@@ -8,8 +8,9 @@ def input_students
     # while the name is not empty, reapeat this code
     while !name.empty? do
         # add the student hash to the array
-        students_list << {name: name, cohort: :november}
+        students_list << {name: name, cohort: :november,}
         #get another names from the user
+
         name = gets.chomp
     end
     # return the array of students
@@ -24,7 +25,9 @@ end
 def print(students)
   index = 0
   while index < (students.count)
-    puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    # center each line individually it string slit over several line. Ruby's string class does not support centering a multi-line string.
+    # use split/map/join combination => "our_str".lines.map { |line| line.strip.center(80) }.join("\n")
+    puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)".center(80)
     index += 1
   end
 end
