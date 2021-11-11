65b86b8c50cb23c5ffb6e86e730711d5a35dc846
Sarah <dousse.sarah@gmail.com>
2021-11-11 12:52:43 +0000

    refactoring print_footer() singular and plural conditional

--- directory.rb
+++ directory.rb
@@ -1,23 +1,25 @@
 # STUDENT DIRECTORY AS IT IS AT STEP 7
 def input_students
-    puts "Please ente the neams of the students"
+    puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # create an empty array
     students_list = []
     # get the first name
+    # alternative to chomp(), use gsub look for Regex "\n" and replace it with "" .gsub("\n","")
     name = gets.chomp
     # while the name is not empty, reapeat this code
     while !name.empty? do
-        # add the student hash to the array
+        # add the student hash to the arrays
         students_list << {name: name, cohort: :november}
         #get another names from the user
+        # alternative to chomp(), use gsub look for Regex "\n" and replace it with ""
         name = gets.chomp
     end
     # return the array of students
     students_list
 end
 
-def print_header
+def print_headersss
   puts "The students of Villains Academy"
   puts "--------------"
 end
@@ -29,7 +31,7 @@ def print(students)
 end
 
 def print_footer(names)
-  names.count <= 1 ? puts "Overall, we have #{names.count} great student" : puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end
 
 # nothing is happening until we call the methods
--- directory_exercise.rb
+++ directory_exercise.rb
@@ -4,11 +4,13 @@ def input_students
     # create an empty array
     students_list = []
     # get the first name
-    name = gets.chomp.capitalize
+    # alternative to chomp(), use gsub look for Regex "\n" and replace it with ""
+    name = gets.gsub("\n", "").capitalize
     # while the name is not empty, reapeat this code
     while !name.empty? do
       # get student's cohort
-      cohort = gets.chomp
+      # alternative to chomp(), use gsub look for Regex "\n" and replace it with ""
+      cohort = gets.gsub("\n", "").capitalize
       cohort.empty? ? cohort = :November : cohort = cohort.capitalize.to_sym
       # add the student hash to the array
       students_list << {name: name, cohort: cohort}
@@ -24,28 +26,28 @@ def print_header
   puts "--------------"
 end
 
-# def print(students)
-#   index = 0
-#   while index < (students.count)
-#     # center each line individually it string slit over several line. Ruby's string class does not support centering a multi-line string.
-#     # use split/map/join combination => "our_str".lines.map { |line| line.strip.center(80) }.join("\n")
-#     puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)".center(80)
-#     index += 1
-#   end
-# end
-
-def print_by_cohort(students)
-  students.each do |student_hash|
-    
+def print(students)
+  index = 0
+  while index < (students.count)
+    # center each line individually it string slit over several line. Ruby's string class does not support centering a multi-line string.
+    # use split/map/join combination => "our_str".lines.map { |line| line.strip.center(80) }.join("\n")
+    puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)".center(80)
+    index += 1
   end
 end
 
+# def print_by_cohort(students)
+#   students.each do |student_hash|
+    
+#   end
+# end
+
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great #{names.count > 1 ? "students" : "student"}"
 end
 
 students = input_students
 print_header
-# print(students)
-print_by_cohort(students)
+print(students)
+# print_by_cohort(students)
 print_footer(students)
